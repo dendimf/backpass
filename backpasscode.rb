5b48e312e3129ed643afd28e797c3b91b553fc9e
dendimf <51936888+dendimf@users.noreply.github.com>
2019-06-18 10:30:05 +0800

    Create backpasscode.rb

---
+++ backpasscode.rb
@@ -0,0 +1,11 @@
+# backpasscode
+
+puts "backpasscode"
+
+print "Put your word to backpass a password: "
+beforebackpass = gets
+
+print "Your word before: "
+puts "#{beforebackpass} this is before backpasscode."
+
+pri
